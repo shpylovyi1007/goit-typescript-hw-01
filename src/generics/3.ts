4b641c0515e065cce92d479888a639b3aab744e5
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-08-28 13:43:11 +0300

    Update 3.ts

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,4 +1,4 @@
-function merge<T extends object, U extends object> (objA: T, objB: U) {
+function merge<T extends object, U extends object> (objA: T, objB: U): T & U {
      return Object.assign(objA, objB);
     }
     
